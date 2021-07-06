a7b2bf5a59f6046165702fac62ccb780f5ed7af5
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-07-06 21:51:48 +0100

    adds cover image upload

--- src/views/new/index.jsx
+++ src/views/new/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { createRef } from "react";
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
 import { Container, Form, Button } from "react-bootstrap";
@@ -20,7 +21,7 @@ export default class NewBlogPost extends Component {
     newPost: {
       category: "",
       title: "",
-      cover:"https://via.placeholder.com/600x600?text=Blog+Image",
+      cover:"",
       readTime: {
         value: 2,
         unit: "minutes"
@@ -30,7 +31,8 @@ export default class NewBlogPost extends Component {
         avatar:"https://res.cloudinary.com/dldkxppwm/image/upload/v1625083348/authors/i9njqmmskahkoo521mox.png"
       },
       content: "",
-    }
+    },
+    coverFile: ""
   }
 
   handleChange(e) {
@@ -51,10 +53,19 @@ export default class NewBlogPost extends Component {
     });
   }
 
+  fileInputRef = createRef()
+
+  handleFile = () => {
+    this.fileInputRef.current.click()
+  }
+
   postNewBlog = async (e) => {
     e.preventDefault() 
 
     try {
+      const newCover = new FormData()
+      newCover.append('cover', this.state.coverFile)
+
       const response = await fetch('http://localhost:3001/blogPosts/', {
         method: 'POST',
         body: JSON.stringify(this.state.newPost),
@@ -62,8 +73,15 @@ export default class NewBlogPost extends Component {
           "Content-type": "application/json"
         }
       })
-      // const postsArray = await response.json()
-      console.log(response)
+      const post= await response.json()
+      console.log(post._id)
+
+      // this.state.coverFile && (
+      //   await fetch(`http://localhost:3001/blogPosts/${post._id}/uploadCover`, {
+      //     method: 'POST',
+      //     body: newCover,
+      //   })
+      // )
       this.props.history.push("/")
       
     } catch (error) {
@@ -97,6 +115,32 @@ export default class NewBlogPost extends Component {
               value={this.state.newPost.content}
               onChange={this.handleQuillChange }
               className="new-blog-content"
+            />
+          </Form.Group>
+          <Form.Group className="mt-3">
+            <Form.Control
+              type="file"
+              hidden
+              ref={this.fileInputRef}
+              onChange={e => this.setState({ coverFile: e.currentTarget.files[0] })}
+            />
+            <Button variant="dark" className="mr-4" onClick={this.handleFile}>
+              Upload Cover
+            </Button>
+            <Button variant="outline-dark" 
+            onClick={() => this.setState({ coverFile: "" })}
+            >
+              Reset Cover
+            </Button>
+            <img
+              src={
+                this.state.coverFile
+                  ? URL.createObjectURL(this.state.coverFile)
+                  : this.state.newPost.cover || "https://via.placeholder.com/600x600?text=Blog+Image"
+              }
+              height="200px"
+              alt="cover"
+              className="d-block mt-4"
             />
           </Form.Group>
           <Form.Group className="d-flex mt-3 justify-content-end">
